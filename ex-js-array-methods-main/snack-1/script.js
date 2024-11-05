66165a088f3d9bda0acf6f7a73c4f281603777cf
PieStw <piero.stalteri@icloud.com>
2024-11-05 13:08:34 +0100

    primo esercizio

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,5 +1,10 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
 
+names.forEach((name) => {
+    console.log(name);
+})
+
+
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
