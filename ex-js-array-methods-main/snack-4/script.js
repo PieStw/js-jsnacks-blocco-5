f6d508008798a3518165afd12108c0e224acecfa
PieStw <piero.stalteri@icloud.com>
2024-11-05 13:14:09 +0100

    esercizio 4

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -5,5 +5,8 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
+const name = posts.map((name) => name.author);
+console.log(name);
+
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
